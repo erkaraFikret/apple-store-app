7501c2732c997983387f7c4231a03ffb51d0e1fb
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-02-27 10:24:58 +0300

    feat : created navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react"
 import Controls from "./components/Controls"
+import Navbar from "./components/Navbar"
 
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
+  const [activePage, setActivePage] = useState(0)
 
   const toggleZoom = () => {
     setFrameZoom(!frameZoom)
+  }
+
+  const handleNavClick = (pageIndex) => {
+    setActivePage(pageIndex)
   }
 
 
@@ -13,6 +19,7 @@ const App = () => {
     <div className='w-full h-screen grid place-items-center'>
       <div className={`${frameZoom && "min-w-[97vw] min-h-[97vh]"} w-[70vw] h-[80vh] min-w-[70vh] min-h-[85vh] max-w-[80vw] max-h-[90vh] 
       border border-gray-300 roundec-2xl resize overflow-auto relative transition-all duration-100`}>
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
       </div>
     </div>
