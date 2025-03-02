330601e163e5632b0d9ee56dcedf6e686d1c5cca
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-03-02 13:14:07 +0300

    feat: finalize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Controls from "./components/Controls"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
@@ -11,9 +11,29 @@ import PageTransition from "./components/PageTransition"
 const App = () => {
   const [frameZoom, setFrameZoom] = useState(false)
   const [activePage, setActivePage] = useState(0)
+  const [isLgScreen, setIsLgScreen] = useState(window.innerWidth > 1024)
+  const [isNavbarOpen, setIsNavbarOpen] = useState(true)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLgScreen(window.innerWidth >= 1024);
+      if (window.innerWidth < 1024) {
+        setFrameZoom(true);
+      } else {
+        setFrameZoom(false);
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const toggleZoom = () => {
-    setFrameZoom(!frameZoom)
+    if (isLgScreen) {
+      setFrameZoom(!frameZoom)
+    }
   }
 
   const handleNavClick = (pageIndex) => {
@@ -23,16 +43,20 @@ const App = () => {
     setActivePage(0)
   }
 
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen)
+  }
+
 
   return (
     <div className='w-full h-screen grid place-items-center'>
       <div className={`${frameZoom && "min-w-[97vw] min-h-[97vh]"} w-[70vw] h-[80vh] min-w-[70vh] min-h-[85vh] max-w-[80vw] max-h-[90vh] 
       border border-gray-300 roundec-2xl resize overflow-auto relative transition-all duration-100 flex`}>
-        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
-        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} resetPage={resetPage} activePage={activePage}/>
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} toggleNavbar={toggleNavbar} isNavbarOpen={isNavbarOpen}/>
+        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} resetPage={resetPage} activePage={activePage} />
         <div className="flex-grow">
           <PageTransition activePage={activePage}>
-            <Home onNavigate={handleNavClick}/>
+            <Home onNavigate={handleNavClick} />
             <IPhone />
             <MacBook />
             <Watch />
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,7 +2,7 @@
 const Controls = ({ toggleZoom, frameZoom, resetPage, activePage }) => {
     return (
         <div className="absolute top-3 right-3 space-x-2 z-10">
-            <button className="text-2xl text-pink-400 cursor-pointer" onClick={toggleZoom}>
+            <button className="text-2xl text-pink-400 cursor-pointer hidden lg:inline-block" onClick={toggleZoom}>
                 <i className={frameZoom ? "bx bxs-zoom-out" : "bx bxs-zoom-in"}></i>
             </button>
             <button className={`text-2xl ${activePage === 0 ? "text-pink-200 cursor-not-allowed" : "text-pink-400"}`} onClick={resetPage}>
